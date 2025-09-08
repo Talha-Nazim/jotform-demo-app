517ac311b71ee6030ed8671445c4d1ce8933d6a3
Talha-Nazim <126316644+Talha-Nazim@users.noreply.github.com>
2025-09-09 00:05:29 +0500

    Fix vercel deployment

--- src/index.js
+++ src/index.js
@@ -108,6 +108,8 @@ app.get("/forms", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("🚀 Server running on http://localhost:5000");
-});
+// app.listen(5000, () => {
+//   console.log("🚀 Server running on http://localhost:5000");
+// });
+
+export default app;
